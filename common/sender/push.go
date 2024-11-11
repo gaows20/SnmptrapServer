a5cb4a1d5419ac3fde24ac9092927c1905ce299e
jmli12 <jmli12@iflytek.com>
2024-11-11 11:13:21 +0800

    mib识别更新1

--- common/sender/push.go
+++ common/sender/push.go
@@ -13,8 +13,8 @@ func PushMetrics(host string, msg global.PushMessage, msg_info string) (err erro
 		Help: "snmp trap info from machine.contains OID, Type,Vale,Timestamp",
 	})
 	completionTime.SetToCurrentTime()
-	completionTime.Set(1) // set可以设置任意值（float64）
-	pusher := push.New(global.GVA_CONFIG.Sender.PushGatewayUrl, global.GVA_CONFIG.Sender.JobName).Collector(completionTime).Grouping("instance", host)
+	completionTime.Set(1) // set可以设置任意值（float64）.BasicAuth("admin", "adgasgbsdfgas")
+	pusher := push.New(global.GVA_CONFIG.Sender.PushGatewayUrl, global.GVA_CONFIG.Sender.JobName).Collector(completionTime).Grouping("instance", host
 	if err := pusher.Push(); err != nil {
 		return err
 	}
